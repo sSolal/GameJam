26c35c402ae3b2eecdeee68f0427a96455866758
PierreGillain <pierre@gillain.org>
2019-05-18 00:00:55 +0200

    RUN ADD

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ var speed = 100#85
 var jumpForce = 400
 var sideJumpForce = 280
 var flag = false
+var run = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -22,12 +23,33 @@ func _process(delta):
 	if abs(velocity.x)<25:
 		if $Body.animation!="idle":
 			$Body.play("idle")
-	elif $Body.animation!="run":
+	elif run == true:
 		$Body.play("run")
+	
+	else:
+		$Body.play("idle")
+		#pour le moment car on a pas d'anim quand il marche
+	
 	if Input.is_action_pressed("ui_left") and not flag:
 		velocity.x = -speed
+		if Input.is_action_pressed("ui_run"):
+			run = true
+			$Body.play("run")
+			velocity.x = velocity.x*1.7
+		if Input.is_action_just_released("ui_run"):
+			run = false
+			$Body.play("run")
+			velocity.x = velocity.x/1.7
 	if Input.is_action_pressed("ui_right") and not flag:
 		velocity.x = speed
+		if Input.is_action_pressed("ui_run"):
+			run = true
+			$Body.play("run")
+			velocity.x = velocity.x*1.7
+		if Input.is_action_just_released("ui_run"):
+			run = false
+			$Body.play("run")
+			velocity.x = velocity.x/1.7
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y=-jumpForce
@@ -37,3 +59,6 @@ func _process(delta):
 			flag = true
 	if is_on_floor() or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
 		flag = false
+	if run == true:
+		run = false
+		
