cef77ddb1d8b4e5e8a8273f68d3221c44329db51
NightySide <nightyside@gmail.com>
2019-05-17 20:26:57 +0200

    Intégré le personnage

--- Player.gd
+++ Player.gd
@@ -5,9 +5,9 @@ extends KinematicBody2D
 # var b = "text"
 var velocity = Vector2(0,0)
 var gravity = 981*2
-var speed = 200
-var jumpForce = 500
-var sideJumpForce = 320
+var speed = 85
+var jumpForce = 400
+var sideJumpForce = 280
 var flag = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +18,7 @@ func _process(delta):
 	velocity.y+=981*delta
 	velocity = move_and_slide(velocity,Vector2(0,-1))
 	velocity.x = velocity.x*0.95
+	$Body.flip_h = velocity.x<0
 	if Input.is_action_pressed("ui_left") and not flag:
 		velocity.x = -speed
 	if Input.is_action_pressed("ui_right") and not flag:
@@ -27,7 +28,7 @@ func _process(delta):
 			velocity.y=-jumpForce
 		elif is_on_wall():
 			velocity.y=-sideJumpForce	
-			velocity.x=sideJumpForce
+			velocity.x=-sign(velocity.x)*sideJumpForce
 			flag = true
 	if is_on_floor() or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
 		flag = false
