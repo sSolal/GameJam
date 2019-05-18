3bd7c33f75de08c4f5b73b5afda1a664a8f1367e
sSolal <stern.solal@gmail.com>
2019-05-18 09:57:27 +0200

    Ajouté animations

--- Player.gd
+++ Player.gd
@@ -8,19 +8,25 @@ var gravity = 981*2
 var speed = 100#85
 var jumpForce = 400
 var sideJumpForce = 280
+var run_speed = 1.7
 var flag = false
 var run = false
+
+var anim
+var current_anim = "idle"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	anim = $AnimationTree.get("parameters/playback")
+	anim.start("idle")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.y+=981*delta
 	velocity = move_and_slide(velocity,Vector2(0,-1))
 	velocity.x = velocity.x*0.95
 	$Body.flip_h = velocity.x<0
-	if abs(velocity.x)<25:
+	
+	
+	"""if abs(velocity.x)<25:
 		if $Body.animation!="idle":
 			$Body.play("idle")
 	elif run == true:
@@ -29,36 +35,66 @@ func _process(delta):
 	else:
 		$Body.play("idle")
 		#pour le moment car on a pas d'anim quand il marche
-	
+	"""
 	if Input.is_action_pressed("ui_left") and not flag:
 		velocity.x = -speed
 		if Input.is_action_pressed("ui_run"):
 			run = true
-			$Body.play("run")
-			velocity.x = velocity.x*1.7
+			#$Body.play("run")
+			#anim.travel("Run")
+			velocity.x = velocity.x*run_speed
 		if Input.is_action_just_released("ui_run"):
 			run = false
-			$Body.play("run")
+			#$Body.play("run")
+			#anim.travel("idle")
 			velocity.x = velocity.x/1.7
 	if Input.is_action_pressed("ui_right") and not flag:
 		velocity.x = speed
 		if Input.is_action_pressed("ui_run"):
 			run = true
-			$Body.play("run")
-			velocity.x = velocity.x*1.7
+			#Body.play("run")
+			#anim.travel("Run")
+			velocity.x = velocity.x*run_speed
 		if Input.is_action_just_released("ui_run"):
 			run = false
-			$Body.play("run")
-			velocity.x = velocity.x/1.7
+			#$Body.play("run")
+			#anim.travel("idle")
+			velocity.x = velocity.x/run_speed
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y=-jumpForce
+			#anim.travel("Jump")
 		elif is_on_wall():
 			velocity.y=-sideJumpForce	
 			velocity.x=-sign(velocity.x)*sideJumpForce
 			flag = true
+			
+	if is_on_floor():
+		if abs(velocity.x)<25:
+			anim.travel("idle")
+	else:
+		if velocity.y>12:
+			anim.travel("Fall")
+		
 	if is_on_floor() or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
 		flag = false
 	if run == true:
 		run = false
+	var next_anim = v_to_anim()
+	if next_anim != current_anim:
+		print(next_anim)
+		anim.travel(next_anim)
+		current_anim = next_anim
+		
+func v_to_anim():
+	if is_on_floor():
+		if abs(velocity.x)>=speed*run_speed:
+			return "Run"
+		else:
+			return "idle"
+	else:
+		if velocity.y>20:
+			return "Fall"
+		else:
+			return "Jump"
 		
