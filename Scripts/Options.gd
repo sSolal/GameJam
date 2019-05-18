70ac98e78dff39363f1caf6063fd6615ca3fc551
sSolal <stern.solal@gmail.com>
2019-05-18 21:03:09 +0200

    Fixed Option volume

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -6,7 +6,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 1)
+	#AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 1)
+	$VSlider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))*5
+	print(AudioServer.get_bus_volume_db(1))
+	$CheckBox.pressed = OS.window_fullscreen
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -19,3 +22,6 @@ func _on_VSlider_value_changed(volume):
 		AudioServer.set_bus_mute(0,true)
 	else:
 		AudioServer.set_bus_mute(0,false)
+
+func EnableFullscreen(button_pressed):
+	OS.window_fullscreen = button_pressed
