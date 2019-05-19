b90e4d1b137dfd950a196f812614d72ff0d1466f
sSolal <stern.solal@gmail.com>
2019-05-19 13:01:50 +0200

    Fixed

--- Scripts/Resizer.gd
+++ Scripts/Resizer.gd
@@ -3,13 +3,17 @@ extends CanvasLayer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export (bool) var mode = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().get_root().connect("size_changed", self, "resize")
 	resize()
 func resize():
-	var ratiox = get_parent().get_viewport_rect().size.x/1024
-	var ratioy = get_parent().get_viewport_rect().size.y/600
-	var ratio = max(ratiox,ratioy)
+	var ratiox = OS.window_size.x/1024
+	var ratioy = OS.window_size.y/600
+	var ratio = 0
+	if mode:
+		ratio = max(ratiox,ratioy)
+	else:
+		ratio = ratioy
 	scale = Vector2(1,1)*ratio
